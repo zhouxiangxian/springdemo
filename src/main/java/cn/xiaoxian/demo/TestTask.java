74cf1728a906b16a2bb475ec9a96d01870f60514
'mldn' <'mldnqa@163.com'>
2019-01-06 17:29:30 +0800

    spring aop编程

--- src/main/java/cn/xiaoxian/demo/TestTask.java
+++ src/main/java/cn/xiaoxian/demo/TestTask.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import cn.xiaoxian.util.ResourceUtils;
-
 public class TestTask {
 	public static void main(String[] args) throws IOException {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
--- src/test/java/cn/mldn/test/TestMessage.java
+++ src/test/java/cn/mldn/test/TestMessage.java
@@ -12,40 +12,12 @@ import cn.xiaoxian.vo.Emp;
 import junit.framework.TestCase;
 
 public class TestMessage {
-	private static ApplicationContext ctx = null;
-	static { // 静态代码块优先于所有的代码执行，目的是为了静态属性初始化
-		ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+	public static void main(String[] args) {
+		
+		 ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+	     cn.xiaoxian.service.IMessageService bean = ctx.getBean("messageServiceImpl",cn.xiaoxian.service.IMessageService.class);
+	     bean.remove("zhou xiang xian ");
 	}
 
-	@Test
-	public void getGetInfo() {
-		IMessageService bean = ctx.getBean("msg", IMessageService.class);
-		Logger.getLogger(TestMessage.class).info(bean.getInfo());
-		TestCase.assertEquals(bean.getInfo(), "www.xiaoxian.com");
-	}
-
-	@Test
-	public void testDeptConstructor() {
-		Emp emp = ctx.getBean("emp", Emp.class);
-		Logger.getLogger(TestMessage.class).info(emp);
-	}
 	
-	
-	@Test
-	public void testDeptXiaoXianConstructor() {
-		Dept dept = ctx.getBean("dept", Dept.class);
-		Logger.getLogger(TestMessage.class).info(dept);
-	}
-	
-	
-	@Test
-	public void testAnnotation() {
-		Dept vo=new Dept();
-		vo.setDeptno(10);
-		vo.setEmps(null);
-		vo.setDname("开发一部");
-		vo.setLoc("深圳");
-		IDeptService deptService = ctx.getBean("deptServiceImpl", IDeptService.class);
-		Logger.getLogger(TestMessage.class).info(deptService.add(vo));
-	}
 }
